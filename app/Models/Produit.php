848e3261d229b409af9d1dc1adf4252115832454
Nawel <benbachirnawel@gmail.com>
2025-10-13 12:04:53 +0200

    modif doc

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produit extends Model
 {
-    //
+    'id', 'code_societe', 'code_produit', 'code_comptable', 'description', 'prix_ht', 'tva', 'qt_stock', 'categorie'];
 }
