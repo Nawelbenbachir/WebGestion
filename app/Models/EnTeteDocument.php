64af00edc2eeea78e8316701b59a6813fefa18da
Nawel <benbachirnawel@gmail.com>
2025-10-13 11:11:48 +0200

    ajout doc

--- app/Models/EnTeteDocument.php
+++ app/Models/EnTeteDocument.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class EnTeteDocument extends Model
 {
-    //
+    protected $fillable = [
+        'type_document',
+        'date_document',
+        'client_id',
+        'total_ht',
+        'total_ttc',
+        'tva',
+        'statut',
+        'remise',
+        'commentaire',
+    ];
 }
