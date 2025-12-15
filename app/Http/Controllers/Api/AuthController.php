f4bf4f095f5d5e53d276973ab82b24ee69d91ce1
Nawel <benbachirnawel@gmail.com>
2025-12-15 16:06:14 +0100

    debut sécurisation api stat

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validData= $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8|confirmed',
+        // Il devra y avoir un champ password_confirmation dans la requête
+        ]);
+        // Hash the password
+        $validData['password'] = bcrypt($validData['password']);
+        // Save the user
+        $user = User::create($validData);
+        // Create token
+        $token = $user->createToken($request->email);
+        return response()->json(['user' => $user, 'token' => $token->plainTextToken], 201);
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+        'email' => 'required|string|exists:users,email',
+        'password' => 'required',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !\Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Identifiants invalides'],
+        401);
+        }
+        $token = $user->createToken($request->email);
+        return response()->json(['user' => $user, 'token' => $token->plainTextToken], 200);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Déconnexion réussie'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StatistiqueController; 
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\AuthController;
 
 
-Route::get('stat-vente/produit/{idProduit}/{annee}/{mois}', [StatistiqueController::class, 'ventesParProduitEtMois']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 
-Route::get('stat-vente/produit/{idProduit}', [StatistiqueController::class, 'ventesParProduitGlobal']);
+    Route::get('stat-vente/produit/{idProduit}/{annee}/{mois}', [StatistiqueController::class, 'ventesParProduitEtMois']);
 
 
-Route::get('stat-vente/periode/{annee}/{mois}', [StatistiqueController::class, 'ventesParPeriodeGlobal']);
+    Route::get('stat-vente/produit/{idProduit}', [StatistiqueController::class, 'ventesParProduitGlobal']);
+
+
+    Route::get('stat-vente/periode/{annee}/{mois}', [StatistiqueController::class, 'ventesParPeriodeGlobal']);
